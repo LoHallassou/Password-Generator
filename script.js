750757e01e89d730614f863054363709fd9c1e18
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-01-09 15:13:26 +0000

    Adding random element selector

--- script.js
+++ script.js
@@ -120,12 +120,13 @@ function getPasswordOptions() {
 // Function for getting a random element from an array
 function getRandom(arr) {
  
-  
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword() { 
 
 }
 
